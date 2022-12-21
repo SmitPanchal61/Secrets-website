126a58725b62429ef0ef3f3d3e82849f30c2804b
Smit panchal <90374408+SmitPanchal61@users.noreply.github.com>
2022-12-21 18:01:44 +0530

    added hashing using md5 algorithm npm package

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -24,7 +25,7 @@ const userSchema = new mongoose.Schema({
 // it encrypts when save() is run and decrypts when find() is run so no need to change any more existing code that queries the database.
 // code should be included before creating a model for that schema
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
 
 const User = mongoose.model("User", userSchema);
 
@@ -44,7 +45,7 @@ app.post("/register", function(req, res) {
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -60,7 +61,7 @@ app.post("/register", function(req, res) {
 app.post("/login", function(req, res) {
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser) {
         if(err) {
